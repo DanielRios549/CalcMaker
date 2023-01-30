389e28fbc7994c551d3c53affeddd9cf473a4561
Daniel Rios <danielrios.oficial@gmail.com>
2023-01-30 19:18:29 -0300

    Add Flet UI

--- main.py
+++ main.py
@@ -1,30 +1,74 @@
+from dataclasses import dataclass
 from random import sample
 from generate import create_a4_paper
+from flet import Page, Row, IconButton, TextField
+import flet
 
-start = int(input("What's the minimal number: "))
-end = int(input("What's the maximum number: ")) + 1
-numbers = range(start, end)
 
-# + Plus (Addition)
-# - Minus (Subtraction)
-# ÷ Obelus (Division)
-# x Times (Multiplication)
-operators = ('+', '-', '÷', 'x')
-operations = []
+@dataclass()
+class CalcMaker():
+    page: Page
 
-for number in range(0, 16):
-    choose = sample(numbers, 2)
-    operation = []
+    def __post_init__(self):
+        self.page.title = "Calc Maker"
+        self.page.horizontal_alignment = flet.CrossAxisAlignment.CENTER
+        self.buttons()
 
-    operation.append(f'{choose[0]:>4}')
-    operation.append(f'{operators[0]:<1} {choose[1]:>2}')
-    operation.append('-' * 6)
+    def buttons(self):
+        self.start = TextField(
+            label="Minimal",
+            hint_text="What's the minimal number: ",
+            keyboard_type=flet.KeyboardType.NUMBER
+        )
+        self.end = TextField(
+            label="Maximum",
+            hint_text="What's the maximum number: ",
+            keyboard_type=flet.KeyboardType.NUMBER
+        )
 
-    operations.append(operation)
+        generate = IconButton(flet.icons.CREATE, on_click=self.operations)
 
-    # print(f'{choose[0]:>4}')
-    # print(f'{operators[0]:<1} {choose[1]:>2}')
-    # print('-' * 4)
+        self.page.add(Row(
+            [
+                self.start,
+                self.end,
+                generate
+            ]
+        ))
 
-create_a4_paper('operations.pdf', operations)
-# create_a4_paper2('operations.pdf')
+        self.page.update()
+
+    def operations(self, event: flet.Event):
+        print(event)
+        # + Plus (Addition)
+        # - Minus (Subtraction)
+        # ÷ Obelus (Division)
+        # x Times (Multiplication)
+        operators = ('+', '-', '÷', 'x')
+        operations = []
+
+        numbers = range(int(self.start.value or 2), int(self.end.value or 6))
+
+        for number in range(0, 16):
+            choose = sample(numbers, 2)
+            operation = []
+
+            operation.append(f'{choose[0]:>4}')
+            operation.append(f'{operators[0]:<1} {choose[1]:>2}')
+            operation.append('-' * 6)
+
+            operations.append(operation)
+
+            # print(f'{choose[0]:>4}')
+            # print(f'{operators[0]:<1} {choose[1]:>2}')
+            # print('-' * 4)
+
+        create_a4_paper('operations.pdf', operations)
+        # create_a4_paper2('operations.pdf')
+
+        self.page.update()
+
+
+if __name__ == "__main__":
+    flet.app(port=5500, target=CalcMaker, view=None)
+    # app(target=CalcMaker)
