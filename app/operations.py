c956030deffbd28ef9f08209061a699d896a7a15
Daniel Rios <danielrios.oficial@gmail.com>
2023-02-04 16:56:18 -0300

    Add Save Button

--- app/operations.py
+++ app/operations.py
@@ -35,63 +35,72 @@ class Operations():
             # x Times (Multiplication)
             signals = ('+', '-', 'x', '÷')
             operators: list[str] = []
-            operations: list[list[str]] = []
+            self.operations: list[list[str]] = []
 
             for index, selection in enumerate(self.main.buttons.operands):
                 if selection.value is True:
                     operators.append(signals[index])
 
-            for index in range(0, 16):
-                operands = sample(numbers, 2)
-                operator = sample(operators, 1)
+            if len(operators) < 1:
+                self.main.message.value = 'Choose at least one Operation'
 
-                if '-' in operator or '÷' in operator:
-                    operands.sort()
-                    operands.reverse()
+            else:
+                for index in range(0, 16):
+                    operands = sample(numbers, 2)
+                    operator = sample(operators, 1)
 
-                operations.append([])
-                operations[index].append(f'{operands[0]:>4}')
-                operations[index].append(f'{operator[0]:<1} {operands[1]:>2}')
-                operations[index].append('-' * 6)
+                    if '-' in operator or '÷' in operator:
+                        operands.sort()
+                        operands.reverse()
 
-                self.main.result.controls.append(
-                    flet.Container(
-                        flet.Column(
-                            [
-                                flet.Row(
-                                    [
-                                        flet.Text(
-                                            f'{operands[0]}',
-                                            size=20
-                                        ),
-                                    ],
-                                    alignment=flet.MainAxisAlignment.END,
-                                    height=30
-                                ),
-                                flet.Row(
-                                    [
-                                        flet.Text(
-                                            f'{operator[0]}   {operands[1]}',
-                                            size=20,
-                                        ),
-                                    ],
-                                    alignment=flet.MainAxisAlignment.END,
-                                    height=30
-                                )
-                            ]
-                        ),
-                        margin=flet.margin.only(bottom=50),
-                        border=flet.border.only(
-                            bottom=flet.border.BorderSide(1, flet.colors.WHITE)
-                        ),
-                        alignment=flet.alignment.center,
-                        col=app.CheckPoints().preview(),
-                        expand=False,
-                        width=30,
-                        height=70
+                    self.operations.append([])
+                    self.operations[index].append(f'{operands[0]:>4}')
+                    self.operations[index].append(f'{operator[0]:<1} {operands[1]:>2}')
+                    self.operations[index].append('-' * 6)
+
+                    self.main.result.controls.append(
+                        flet.Container(
+                            flet.Column(
+                                [
+                                    flet.Row(
+                                        [
+                                            flet.Text(
+                                                f'{operands[0]}',
+                                                size=20
+                                            ),
+                                        ],
+                                        alignment=flet.MainAxisAlignment.END,
+                                        height=30
+                                    ),
+                                    flet.Row(
+                                        [
+                                            flet.Text(
+                                                f'{operator[0]}   {operands[1]}',
+                                                size=20,
+                                            ),
+                                        ],
+                                        alignment=flet.MainAxisAlignment.END,
+                                        height=30
+                                    )
+                                ]
+                            ),
+                            margin=flet.margin.only(bottom=50),
+                            border=flet.border.only(
+                                bottom=flet.border.BorderSide(1, flet.colors.WHITE)
+                            ),
+                            alignment=flet.alignment.center,
+                            col=app.CheckPoints().preview(),
+                            expand=False,
+                            width=30,
+                            height=70
+                        )
                     )
+
+                self.main.page.add(
+                    flet.ElevatedButton('Save', icon=flet.icons.SAVE, on_click=self.save)
                 )
 
             self.main.page.update()
-            generate = app.Generate('operations.pdf', operations)
-            generate.create_a4_paper()
+
+    def save(self, event: flet.Event):
+        app.Generate('operations.pdf', self.operations).create_a4_paper()
